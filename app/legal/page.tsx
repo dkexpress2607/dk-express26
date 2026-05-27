88c858bdb832310d64ca37bb566cb6cffdbace6d
DK Express 26 <dkexpress2607@gmail.com>
2026-05-27 16:14:38 +0200

    Ajout page légale

---
+++ app/legal/page.tsx
@@ -0,0 +1,10 @@
+<footer className="bg-gray-100 py-8 text-center">
+
+  <a
+    href="/legal"
+    className="text-blue-700 font-semibold hover:underline"
+  >
+    Conditions & Informations légales
+  </a>
+
+</footer>
